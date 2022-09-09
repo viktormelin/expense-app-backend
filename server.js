61ecabd3cf3aa47f2c14c161918df91223c094a9
noahlissner <noah.lissner@gmail.com>
2022-09-09 23:25:23 +0200

    feat: Added register & login routes

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
   console.log(`✅ Server running on port ${port}`);
