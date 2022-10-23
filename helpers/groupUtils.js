b0b05a7d16128437972e3ef6b38f566695d85c8c
dough <viktor.melin@me.com>
2022-10-23 22:27:54 +0200

    fix: prisma shit

--- helpers/groupUtils.js
+++ helpers/groupUtils.js
@@ -1,51 +1,51 @@
+const Users = require('../models/userModel');
+
 const getUserFromEmail = async (email) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  });
+	const user = await Users.findOne({
+		email,
+	});
 
-  if (user) {
-    return user.id;
-  } else {
-    throw new Error("Could not find user");
-  }
+	if (user) {
+		return user.id;
+	} else {
+		throw new Error('Could not find user');
+	}
 };
 
-const getUserFromId = async (id) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      id,
-    },
-  });
+// const getUserFromId = async (id) => {
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       id,
+//     },
+//   });
 
-  if (user) {
-    return user;
-  } else {
-    throw new Error("Could not find user");
-  }
-};
+//   if (user) {
+//     return user;
+//   } else {
+//     throw new Error("Could not find user");
+//   }
+// };
 
 const createMembersArray = async (userId, users) => {
-  let tempArr = [];
+	let tempArr = [];
 
-  if (users) {
-    for (const email of users) {
-      const member = await getUserFromEmail(email);
-      if (member) {
-        tempArr.push(member);
-      }
-    }
+	if (users) {
+		for (const email of users) {
+			const member = await getUserFromEmail(email);
+			if (member) {
+				tempArr.push(member);
+			}
+		}
 
-    tempArr.unshift(userId);
-    return tempArr;
-  } else {
-    tempArr.push(userId);
-    return tempArr;
-  }
+		tempArr.unshift(userId);
+		return tempArr;
+	} else {
+		tempArr.push(userId);
+		return tempArr;
+	}
 };
 
 module.exports = {
-  getUserFromEmail,
-  createMembersArray,
+	getUserFromEmail,
+	createMembersArray,
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,47 +1,48 @@
-const jwt = require("jsonwebtoken");
-const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const Users = require('../models/userModel');
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+	let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(" ")[1];
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		try {
+			// Get token from header
+			token = req.headers.authorization.split(' ')[1];
 
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+			// Verify token
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
+			// Get user from the token
 
-      const user = await prisma.user.findFirst({
-        where: { id: decoded.id },
-      });
+			const user = await Users.findOne({
+				id: decoded.id,
+			});
 
-      req.user = {
-        id: user.id,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
-        groups: user.groups,
-        phone: user.phone,
-      };
+			req.user = {
+				id: user.id,
+				firstname: user.firstname,
+				lastname: user.lastname,
+				email: user.email,
+				groups: user.groups,
+				phone: user.phone,
+			};
 
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error("Not authorized");
-    }
-  }
+			next();
+		} catch (error) {
+			console.log(error);
+			res.status(401);
+			throw new Error('Not authorized');
+		}
+	}
 
-  if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
-  }
+	if (!token) {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
 });
 
 module.exports = { protect };
